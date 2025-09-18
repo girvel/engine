2070df4adb113f5087043ade636d07efd178e2be
girvel <widauka@ya.ru>
2025-09-18 18:07:36 +0500

    Parametrize frequency for wandering AI

--- mech/wandering_ai.lua
+++ mech/wandering_ai.lua
@@ -8,13 +8,14 @@ local wandering_ai = {}
 local methods = {}
 wandering_ai.mt = {__index = methods}
 
+--- @param frequency_k? number
 --- @return wandering_ai
-wandering_ai.new = function()
-  return setmetatable({ }, wandering_ai.mt)
+wandering_ai.new = function(frequency_k)
+  return setmetatable({_frequency_k = frequency_k or 1,}, wandering_ai.mt)
 end
 
 methods.control = function(entity)
-  async.sleep(math.random(0.5, 7))
+  async.sleep(math.random(0.5, 7) / entity.ai._frequency_k)
   actions.move(Random.choice(Vector.directions)):act(entity)
 end
 
