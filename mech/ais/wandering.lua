464bbb9d699f4a67bc45e20fc924abb55303543d
girvel <widauka@ya.ru>
2025-09-19 11:13:30 +0500

    Efficient running away logic for ais/wandering.lua

--- mech/ais/wandering.lua
+++ mech/ais/wandering.lua
@@ -7,7 +7,7 @@ local api       = require("engine.tech.api")
 
 local wandering = {}
 
---- @class wandering_ai
+--- @class wandering_ai: ai
 --- @field _frequency_k number
 --- @field _target? entity
 local methods = {}
@@ -23,7 +23,10 @@ end
 methods.init = function(entity)
   State.hostility:subscribe(function(attacker, target)
     if entity.faction and target == entity then
+      local ai = entity.ai  --[[@as wandering_ai]]
       State.hostility:set(entity.faction, attacker.faction, "enemy")
+      ai._target = attacker
+      ai._control_coroutine = nil
     end
   end)
 end
@@ -38,7 +41,7 @@ local TARGET_SEARCH_RANGE = 10
 methods.observe = function(entity, dt)
   local ai = entity.ai  --[[@as wandering_ai]]
   if (not ai._target or (ai._target.position - entity.position):abs2() > TARGET_RANGE)
-    and Random.chance(dt / TARGET_SCAN_PERIOD)
+    and Period(TARGET_SCAN_PERIOD, ai, "target_scan")
   then
     ai._target = tk.find_target(entity, TARGET_SEARCH_RANGE)
   end
@@ -47,7 +50,6 @@ end
 --- @param entity entity
 methods.control = function(entity)
   local ai = entity.ai  --[[@as wandering_ai]]
-  async.sleep(math.random(0.5, 7) / ai._frequency_k)
 
   if ai._target then
     local max_distance = 0
@@ -56,14 +58,15 @@ methods.control = function(entity)
       p:add_mut(entity.position)
       local d = (p - ai._target.position):abs2()
       if State.grids.solids:can_fit(p) and d > max_distance then
-        Log.trace(p)
         max_distance = d
         run_to = p
       end
     end
 
     api.travel(entity, run_to)
+    async.sleep(.5)
   else
+    async.sleep(math.random(0.5, 7) / ai._frequency_k)
     actions.move(Random.choice(Vector.directions)):act(entity)
   end
 end
