f4ca8f22b9d6892d509412b496170f865e70158a
girvel <widauka@ya.ru>
2025-10-12 13:25:11 +0500

    Math.inside_polygon

--- lib/math.lua
+++ lib/math.lua
@@ -34,4 +34,44 @@ mathx.loopmod = function(a, b)
   return (a - 1) % b + 1
 end
 
+--- @param start
+--- @param p1 vector
+--- @param p2 vector
+--- @return boolean
+local probe = function(start, p1, p2)
+  local xr, yr = unpack(start)
+  local x1, y1 = unpack(p1)
+  local x2, y2 = unpack(p2)
+
+  if not ((y1 <= yr and y2 > yr) or (y2 <= yr and y1 > yr)) then
+    return false
+  end
+
+  if y1 == y2 then return yr == y1 and xr <= math.max(x1, x2) end
+
+  local x_intersect
+  if x1 == x2 then
+    x_intersect = x1
+  else
+    local k = (x2 - x1) / (y2 - y1)
+    x_intersect = (yr - y1) * k + x1
+  end
+
+  return x_intersect > xr
+end
+
+--- @param p vector
+--- @param vertices vector[]
+--- @return boolean
+mathx.inside_polygon = function(p, vertices)
+  local count = 0
+  for i = 1, #vertices do
+    if probe(p, vertices[i], vertices[mathx.loopmod(i + 1, #vertices)]) then
+      count = count + 1
+    end
+  end
+
+  return count % 2 == 1
+end
+
 return mathx
