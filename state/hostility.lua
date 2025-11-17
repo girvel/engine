74c780f8c9d071a6ef7d9ec6d3b16b61322b71e1
girvel <widauka@ya.ru>
2025-11-17 17:54:13 +0500

    Fix State.hostility._aggression_subscriptions typo

--- state/hostility.lua
+++ state/hostility.lua
@@ -8,7 +8,7 @@ local hostility = {}
 
 --- @class state_hostility
 --- @field _are_hostile table<string, hostility>
---- @field _agression_subscriptions fun(entity, entity)[]
+--- @field _aggression_subscriptions fun(entity, entity)[]
 local methods = {}
 local mt = {__index = methods}
 
@@ -16,7 +16,7 @@ local mt = {__index = methods}
 hostility.new = function()
   return setmetatable({
     _are_hostile = {},
-    _agression_subscriptions = {},  -- TODO use weak tables?
+    _aggression_subscriptions = {},  -- TODO use weak tables?
   }, mt)
 end
 
@@ -38,9 +38,9 @@ methods.set = function(self, faction_a, faction_b, value)
   local key = faction_a .. "_to_" .. faction_b
   if self._are_hostile[key] == value then return end
 
-  Log.info("%s %s hostile towards %s",
+  Log.info("%s becomes %s towards %s",
     faction_a,
-    value and "becomes" or "stops being",
+    value or "neutral",
     faction_b
   )
   self._are_hostile[key] = value
@@ -50,19 +50,19 @@ end
 --- @return fun(entity, entity)
 methods.subscribe = function(self, f)
   Ldump.ignore_upvalue_size(f)
-  table.insert(self._agression_subscriptions, f)
+  table.insert(self._aggression_subscriptions, f)
   return f
 end
 
 --- @param f fun(entity, entity)
 methods.unsubscribe = function(self, f)
-  Table.remove(self._agression_subscriptions, f)
+  Table.remove(self._aggression_subscriptions, f)
 end
 
 --- @param entity entity the one attacking
 --- @param target entity
 methods.register = function(self, entity, target)
-  for _, f in ipairs(self._agression_subscriptions) do
+  for _, f in ipairs(self._aggression_subscriptions) do
     f(entity, target)
   end
 end
