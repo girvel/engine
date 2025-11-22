52694204ec6b4846a49c19006560dc173a910970
girvel <widauka@ya.ru>
2025-11-22 21:18:09 +0500

    Introduce shaders.build function

--- tech/shaders/water.lua
+++ tech/shaders/water.lua
@@ -1,34 +1,14 @@
+local shaders = require("engine.tech.shaders.init")
+
+
 local water = {}
-
-local build_love_shader
-build_love_shader = function(palette_path, palette_real_colors_n)
-  local result = love.graphics.newShader(string.format(
-    love.filesystem.read("engine/tech/shaders/water.frag"),
-    palette_real_colors_n,
-    palette_real_colors_n
-  ), nil)  --- @diagnostic disable-line
-
-  do
-    local palette = {}
-    local palette_image_data = love.image.newImageData(palette_path)
-    for x = 0, palette_real_colors_n - 1 do
-      table.insert(palette, {palette_image_data:getPixel(x, 0)})
-    end
-    result:send("palette", unpack(palette))
-  end
-
-  Ldump.serializer.handlers[result] = function()
-    return build_love_shader(palette_path, palette_real_colors_n)
-  end
-  return result
-end
 
 --- @param palette_path string
 --- @param palette_real_colors_n number
 --- @return shader
 water.new = Memoize(function(palette_path, palette_real_colors_n)
   return {
-    love_shader = build_love_shader(palette_path, palette_real_colors_n),
+    love_shader = shaders.build("engine/tech/shaders/water.frag"),
 
     preprocess = function(self, entity, dt)
       local offset = ((love.timer.getTime() * entity.water_velocity) % Constants.cell_size):map(math.floor) / Constants.cell_size
@@ -43,11 +23,9 @@ water.new = Memoize(function(palette_path, palette_real_colors_n)
       local reflected = State.grids.solids:slow_get(entity.position + Vector.up)
       if not reflected or reflected.low_flag then return nil end
       return reflected.sprite.image
-    end
+    end,
   }
 end)
 
-Ldump.mark(water, {
-  new = {build_love_shader = {}},
-}, ...)
+Ldump.mark(water, {}, ...)
 return water
