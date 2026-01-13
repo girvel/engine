1a75d9cab1f4a672f7864a2f281d549834e77156
girvel <widauka@ya.ru>
2026-01-13 11:40:36 +0500

    appearance_editor: hair colors

--- state/mode/appearance_editor.lua
+++ state/mode/appearance_editor.lua
@@ -3,27 +3,17 @@ local ui = require("engine.tech.ui")
 local tk = require("engine.state.mode.tk")
 
 
-local KINDS_OF_HAIR = {
-  {
-    codename = "none",
-    name = "Лысый",
-  },
-  {
-    codename = "short_hair_1",
-    name = "Короткие (1)",
-  },
-  {
-    codename = "short_hair_2",
-    name = "Короткие (2)",
-  },
-  {
-    codename = "short_hair_3",
-    name = "Короткие (3)",
-  },
-  {
-    codename = "long_hair",
-    name = "Длинные волосы",
-  },
+local HAIR_TYPES = {
+  {codename = "none", name = "Лысый"},
+  {codename = "short_hair_1", name = "Короткие (1)"},
+  {codename = "short_hair_2", name = "Короткие (2)"},
+  {codename = "short_hair_3", name = "Короткие (3)"},
+}
+
+local HAIR_COLORS = {
+  {codename = "red", name = "Рыжий"},
+  {codename = "gray", name = "Седеющий"},
+  {codename = "brown", name = "Русый"},
 }
 -- NEXT same idiom for abilities in creator
 
@@ -42,7 +32,8 @@ appearance_editor.new = function(prev)
     type = "appearance_editor",
     _prev = prev,
     model = {
-      hair_type = KINDS_OF_HAIR[1],
+      hair_type = HAIR_TYPES[1],
+      hair_color = HAIR_COLORS[2],
     },
   }, appearance_editor.mt)
 end
@@ -61,19 +52,27 @@ methods.draw_gui = function(self, dt)
       ui.start_line()
         ui.selector()
         ui.text("Тип волос:")
-        local hair_type_changed = ui.switch(KINDS_OF_HAIR, self.model, "hair_type")
+        local hair_type_changed = ui.switch(HAIR_TYPES, self.model, "hair_type")
+      ui.finish_line()
+
+      ui.start_line()
+        ui.selector()
+        ui.text("Цвет волос:")
+        local hair_color_changed = ui.switch(HAIR_COLORS, self.model, "hair_color")
       ui.finish_line()
     ui.finish_font()
     ui.finish_frame()
 
-    if hair_type_changed then
+    if hair_type_changed or hair_color_changed then
       local inventory = State.player.inventory
       local hair_type = self.model.hair_type.codename
+      local hair_color = self.model.hair_color.codename
+
       if inventory.hair then
         State:remove(inventory.hair, true)
       end
       inventory.hair = hair_type ~= "none"
-        and State:add(items_entities.hair(hair_type, "red"))
+        and State:add(items_entities.hair(hair_type, hair_color))
         or nil
     end
   tk.finish_window()
